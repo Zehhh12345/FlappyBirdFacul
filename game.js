c11d53417daae6c8f804a42bfcdef4f496db4bdd
José Henrique de Souza <161393887+Zehhh12345@users.noreply.github.com>
2025-06-24 11:38:47 -0300

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ const pipeWidth = 60;
 let animation;
 
 const birdImg = new Image();
-birdImg.src = "https://raw.githubusercontent.com/rafaelreis-hotmail/flappybird-assets/main/bird.png";
+birdImg.src = "https://upload.wikimedia.org/wikipedia/en/0/0a/Flappy_Bird.png";
 
 const bird = {
   x: 100,
